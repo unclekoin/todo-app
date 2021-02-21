abc1590892af2165eb0da6fb36a1514021e7c7d9
Pavel Koryakin <unclekoin@gmail.com>
2021-02-21 13:09:38 +0300

    feat: create structure of project

---
+++ src/components/TodoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TodoListItem from './TodoListItem';
+
+const TodoList = () => {
+  return (
+    <ul>
+      <li><TodoListItem /></li>
+      <li><TodoListItem /></li>
+      <li><TodoListItem /></li>
+    </ul>
+  );
+};
+
+export default TodoList;
--- src/index.js
+++ src/index.js
@@ -1,41 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const AppHeader = () => {
-  return <h1>My Todo List</h1>;
-};
-
-const SearchPanel = () => {
-  const searchText = 'Type here to search...';
-  const searchStyle = {
-    fontSize: '20px'
-  }
-  return <input
-    style={searchStyle}
-    placeholder={searchText} />;
-};
-
-const TodoList = () => {
-  const items = ['Drink Coffee', 'Learn React', 'Build Awesom App'];
-
-  return (
-    <ul>
-      <li>{items[0]}</li>
-      <li>{items[1]}</li>
-      <li>{items[2]}</li>
-    </ul>
-  );
-};
+import AppHeader from './components/AppHeader'
+import SearchPanel from './components/SearchPanel'
+import TodoList from './components/TodoList'
 
 const App = () => {
-  let isLoggedIn = true;
-  const loginBox = <span>Log in please</span>;
-  const welcomeBox = <span>Welcome Back</span>;
-
   return (
     <div>
-      <div>{new Date().toString()}</div>
-      {isLoggedIn ? welcomeBox : loginBox}
       <AppHeader />
       <SearchPanel />
       <TodoList />
