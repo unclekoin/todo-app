cedef468dfe97bb791664141e069077b305229ab
Pavel Koryakin <unclekoin@gmail.com>
2021-02-21 17:28:26 +0300

    feat: add basic styles

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app-header.css'
+
+const AppHeader = () => {
+  return (
+    <div className="app-header d-flex">
+      <h1>My Todo List</h1>
+      <h2>Some text</h2>
+    </div>
+  )
+};
+
+export default AppHeader;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppHeader from './components/AppHeader'
-import SearchPanel from './components/SearchPanel'
-import TodoList from './components/TodoList'
+import App from './components/app'
 
-const App = () => {
-  return (
-    <div>
-      <AppHeader />
-      <SearchPanel />
-      <TodoList />
-    </div>
-  );
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />,
+  document.getElementById('root'));
